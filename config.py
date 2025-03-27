0e6ec9d2064e15aeccdf59af1f8bba544a89b512
ViosRio <erollitex@gmail.com>
2025-03-28 01:16:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ ADMIN_ID = 6629910007 # CHAT İD İLE DEĞİŞTİR
 ALLOWED_USERS_FILE = "allowed_users.txt"
 RUNNING_FILES = "running_files.txt"
 BOT_USERNAME = "LunaQuadBot"
+START_IMG = "https://telegra.ph/file/2b4e22e24548f55f40e65.jpg"
