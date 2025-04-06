1ee3541e01bbdb92d4c03a7a0668246180762871
ViosRio <erollitex@gmail.com>
2025-04-06 03:30:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 TOKEN = "{TOKEN}"
 ADMIN_ID = 6669596825 
-ALLOWED_USERS_FILE = "allowed_users.txt"
+ALLOWED_USERS_FILE = "allowed_users.py"
 RUNNING_FILES = "running_files.txt"
 BOT_USERNAME = "LunaQuadBot"
 START_IMG = "https://telegra.ph/file/2b4e22e24548f55f40e65.jpg"
